725ba4c688b97b9fe060d64283b3e9b8fe3ac1a7
James Becwar <jbecwar@gmail.com>
2025-04-20 11:30:01 -0500

    Update 20250420113001'

--- rss.py
+++ rss.py
@@ -1,11 +1,31 @@
 import feedparser
 import db
+import random
 
 def getTitles(runtime):
-    #url = 'https://www.reddit.com/r/News/.rss'
-    url = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+    urls = [
+    'http://www.reddit.com/.rss',
+    'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+    'https://news.yahoo.com/rss',
+    'https://www.theguardian.com/world/rss',
+    'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en',
+    'https://www.bbc.co.uk/feeds/rss/world.xml',
+    'https://www.washingtonpost.com/rss/',
+    'https://www.cnn.com/services/rss/',
+    'https://www.aljazeera.com/xml/rss/all.xml',
+    'https://www.reuters.com/tools/rss',
+    'https://www.npr.org/rss/rss.php?id=1001',
+    'https://www.foxnews.com/about/rss',
+    'https://www.usatoday.com/rss/'
+    ]
+    
+    entityCount = 0
 
-    feed = feedparser.parse(url)
+    while entityCount <1:
+        url = random.choice(urls)
+        feed = feedparser.parse(url)
+        entityCount = len(feed.entries)
+    
     for entry in feed.entries:
         db.insertHeadline(url, entry.title, entry.link, entry.published, runtime)
     return [entry.title for entry in feed.entries]
