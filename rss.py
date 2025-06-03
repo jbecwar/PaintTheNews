e54f213c1d614ccddf6fb1f88f1df6923cb687cd
James Becwar <jbecwar@gmail.com>
2025-06-03 12:51:21 -0500

    Update 20250603125121

--- rss.py
+++ rss.py
@@ -4,17 +4,18 @@ import random
 
 def getTitles(runtime):
     urls = [
-    'http://www.reddit.com/.rss',
-    'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
-    'https://news.yahoo.com/rss',
-    'https://www.theguardian.com/world/rss',
-    'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en',
-    'https://www.bbc.co.uk/feeds/rss/world.xml',
-    'https://www.cnn.com/services/rss/',
-    'https://www.aljazeera.com/xml/rss/all.xml',
-    'https://www.npr.org/rss/rss.php?id=1001',
-    'https://www.foxnews.com/about/rss',
-    'https://www.usatoday.com/rss/'
+    # 'http://www.reddit.com/.rss',
+    # 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+    # 'https://news.yahoo.com/rss',
+    # 'https://www.theguardian.com/world/rss',
+    # 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en',
+    # 'https://www.bbc.co.uk/feeds/rss/world.xml',
+    # 'https://www.cnn.com/services/rss/',
+    # 'https://www.aljazeera.com/xml/rss/all.xml',
+    # 'https://www.npr.org/rss/rss.php?id=1001',
+    # 'https://www.foxnews.com/about/rss',
+    # 'https://www.usatoday.com/rss/'
+    'https://www.goodnewsnetwork.org/category/news/feed/'
     ]
     
     entityCount = 0
